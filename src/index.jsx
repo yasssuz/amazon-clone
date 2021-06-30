c76acfa106dcee5b8e09d16cdfb951783f32fe76
Karim Yassine <karimwebdevelopment@gmail.com>
2021-06-30 15:38:54 -0300

    added global styles

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 import App from './App';
+
+const GlobalStyles = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  :root {
+    --clr-white: #FFFFFF;
+    --clr-dark-gray: #131921;
+    --clr-dark-orange: #cd9042;
+  }
+`
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
