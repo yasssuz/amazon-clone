d6323541f921996b9dbbacca794dca1d1b74f121
Karim Yassine <karimwebdevelopment@gmail.com>
2021-07-14 23:09:13 -0300

    code refactor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
-import styled from 'styled-components'
-import SearchIcon from '@material-ui/icons/Search'
-import ShoppingBasketIcon  from '@material-ui/icons/ShoppingBasket'
+import styled from "styled-components";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
-
-export function Header() {
+export default function Header() {
   return (
     <HeaderContainer>
-      <Logo src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" />
+      <Logo
+        src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'
+        alt='amazon logo'
+      />
       <SearchArea>
-        <SearchInput type="text"/>
+        <SearchInput type='text' />
         <SearchIcon />
       </SearchArea>
       <Navbar>
@@ -30,7 +32,7 @@ export function Header() {
         </Basket>
       </Navbar>
     </HeaderContainer>
-  )
+  );
 }
 
 const HeaderContainer = styled.header`
@@ -41,14 +43,14 @@ const HeaderContainer = styled.header`
   top: 0;
   z-index: 100;
   background: var(--clr-dark-gray);
-`
+`;
 
 const Logo = styled.img`
   width: 100px;
   object-fit: contain;
   margin: 0 2rem;
   margin-top: 1.8rem;
-`
+`;
 
 const SearchArea = styled.div`
   display: flex;
@@ -61,19 +63,19 @@ const SearchArea = styled.div`
     height: 2.2rem !important;
     background: var(--clr-dark-orange);
   }
-`
+`;
 
 const SearchInput = styled.input`
   height: 1.2rem;
   padding: 1rem;
   border: none;
   width: 100%;
-`
+`;
 
 const Navbar = styled.nav`
   display: flex;
   justify-content: space-evenly;
-`
+`;
 
 const Option = styled.div`
   display: flex;
@@ -89,7 +91,7 @@ const Option = styled.div`
     font-size: 1.3rem;
     font-weight: 800;
   }
-`
+`;
 
 const Basket = styled.div`
   display: flex;
@@ -99,4 +101,4 @@ const Basket = styled.div`
   span {
     margin: 0 1rem;
   }
-`
+`;
